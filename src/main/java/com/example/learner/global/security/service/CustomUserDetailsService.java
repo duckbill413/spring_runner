75a8727f5abe9d5344325ae54020f35da4b38c75
duckbill413 <uhyeon7399@daum.net>
2024-03-08 22:40:11 +0900

    refresh token을 통해 업데이트시 토큰 권한 설정 오류 문제 해결

--- src/main/java/com/example/learner/global/security/service/CustomUserDetailsService.java
+++ src/main/java/com/example/learner/global/security/service/CustomUserDetailsService.java
@@ -1,16 +1,21 @@
 package com.example.learner.global.security.service;
 
 import com.example.learner.domain.member.dao.MemberRepository;
+import com.example.learner.domain.member.entity.MemberRole;
 import com.example.learner.global.common.code.ErrorCode;
 import com.example.learner.global.exception.BaseExceptionHandler;
 import com.example.learner.global.security.user.UserSecurityDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -29,7 +34,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         return UserSecurityDTO.fromSocial()
                 .username(member.getId().toString())
                 .password(UUID.randomUUID().toString())
-                .authorities(List.of())
+                .authorities(getMemberAuthorities(member.getRole()))
                 .create();
+    }
+    private Collection<GrantedAuthority> getMemberAuthorities(Set<MemberRole> role) {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        role.forEach(memberRole -> authorities.add(new SimpleGrantedAuthority("ROLE_" + memberRole)));
+        return authorities;
     }
 }
