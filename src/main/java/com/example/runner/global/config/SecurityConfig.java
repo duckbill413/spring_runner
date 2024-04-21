2ff7500de68a771cc3e59ce522ca56f9d8f81809
duckbill413 <uhyeon7399@daum.net>
2024-04-21 16:27:19 +0900

    register user, admin user

--- src/main/java/com/example/runner/global/config/SecurityConfig.java
+++ src/main/java/com/example/runner/global/config/SecurityConfig.java
@@ -1,27 +1,55 @@
 package com.example.runner.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
     String[] WHITE_LIST_URL = {
             "/webjars/**", "/images/**", "/css/**", "/h2-console/**",
             "/login"
     };
+    private final AccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(WHITE_LIST_URL).permitAll()
+                        .requestMatchers(WHITE_LIST_URL).permitAll() // WHITE_LIST_URL 의 모든 경로에 대하여 접근 허용
+                        .requestMatchers("/delete/**").hasRole("ADMIN") // ADMIN 권한을 가져야 /delete/** 경로 접근 가능
                         .anyRequest().authenticated()
                 ).formLogin(httpSecurityFormLoginConfigurer ->
-                        httpSecurityFormLoginConfigurer.loginPage("/login"))
+                        httpSecurityFormLoginConfigurer.loginPage("/login")
+                ).exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer.accessDeniedHandler(customAccessDeniedHandler)
+                )
         ;
 
         return http.build();
+    }
+    @Bean
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("user")
+                .password(passwordEncoder().encode("duckbill"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("duckbill"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/example/runner/global/security/api/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.example.runner.global.security.api.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendRedirect(request.getContextPath() + "/accessDenied");
+    }
+}
